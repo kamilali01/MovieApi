679d5d62428d184b9446dc7976d1b5c8a8b88dd1
kamilali01 <kamilaliyev20030117@gmail.com>
2025-02-18 15:11:22 +0400

    Application/Handlers Update: Get Category Query Handler and Get Movie Query Handler added. Presentation Update: Category Controller Created

---
+++ Core/MovieApi.Application/Features/CQRSDesingPattern/Handlers/CategoryHandlers/GetCategoryQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.CQRSDesingPattern.Results.CategoryResults;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesingPattern.Handlers.CategoryHandlers
+{
+    public class GetCategoryQueryHandler
+    {
+        private readonly MovieContext _ctx;
+
+        public GetCategoryQueryHandler(MovieContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<List<GetCategoryQueryResult>> Handle()
+        {
+            var values = _ctx.Categories.ToListAsync();
+            return values.Result.Select(x => new GetCategoryQueryResult
+            {
+                Id = x.Id,
+                Name = x.Name
+            }).ToList();
+        }
+    }
+}
---
+++ Core/MovieApi.Application/Features/CQRSDesingPattern/Handlers/MovieHandlers/GetMovieQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.CQRSDesingPattern.Results.MovieResults;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesingPattern.Handlers.MovieHandlers
+{
+    public class GetMovieQueryHandler
+    {
+        private readonly MovieContext _ctx;
+
+        public GetMovieQueryHandler(MovieContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<List<GetMovieQueryResult>> Handle()
+        {
+            var values = _ctx.Movies.ToListAsync();
+            return values.Result.Select(x => new GetMovieQueryResult
+            {
+                Id = x.Id,
+                Title = x.Title,
+                CoverImageUrl = x.CoverImageUrl,
+                Rating = x.Rating,
+                Description = x.Description,
+                Duration = x.Duration,
+                ReleaseDate = x.ReleaseDate,
+                CreatedYear = x.CreatedYear,
+                Status = x.Status
+            }).ToList();
+        }
+}
